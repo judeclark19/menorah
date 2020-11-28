760acc50a7f19729c7ed2400cfd56b74e9cc8f5d
Jude Clark <judeclark2019@gmail.com>
2020-11-28 15:35:26 -0500

    created Menorah 0

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import styled from "styled-components";
 
 //images
+import MenorahEmpty from "./images/Menorah-empty.svg";
 import MenorahFull from "./images/Menorah-full.svg";
 
 function App() {
   return (
     <AppDiv>
-      <MenorahImg>
-        <img src={MenorahFull}></img>
-      </MenorahImg>
+      <MenorahDiv>
+        {/* <img src={MenorahFull} alt="all candles lit"></img> */}
+        <img src={MenorahEmpty} alt="no candles lit"></img>
+        <div className="buttons">
+          <button>On</button>
+          <button>Off</button>
+        </div>
+      </MenorahDiv>
     </AppDiv>
   );
 }
@@ -17,19 +23,32 @@ function App() {
 const AppDiv = styled.div`
   /* border: 3px dashed orange; */
   height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-image: linear-gradient(180deg, #252a26, #1c211d);
+
+  /* background-image: linear-gradient(180deg, #252a26, #1c211d); */
 `;
 
-const MenorahImg = styled.div`
-  height: 80%;
+const MenorahDiv = styled.div`
+  /* height: 80%; */
   /* max-width: 100vw; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 5rem;
+  height: 100%;
+  width: 100%;
   /* border: 2px solid red; */
+
+  @media (max-width: 800px) {
+    padding: 5rem 0rem;
+  }
+
   img {
     height: 100%;
     object-fit: cover;
+  }
+  button {
+    padding: 1rem;
   }
 `;
 
