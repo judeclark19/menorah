ae1c26b26c26f6de712f6a9e03f201d4ead8f769
Jude Clark <judeclark2019@gmail.com>
2020-11-28 16:25:12 -0500

    9-way switch case

--- src/App.js
+++ src/App.js
@@ -4,33 +4,91 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Menorah from "./components/Menorah";
 
-//images
-import MenorahEmpty from "./images/Menorah-empty.svg";
-import MenorahFull from "./images/Menorah-full.svg";
-
 function App() {
   const [candlesLit, setCandlesLit] = useState(0);
+
+  const backgroundColors = [
+    "#020305",
+    "#040609",
+    "#080C12",
+    "#101823",
+    "#182434",
+    "#1F3045",
+    "#26374B",
+    "#2D3D51",
+    "#374659",
+  ];
+
   return (
-    <AppDiv>
-      <MenorahDiv>
+    <AppDiv style={{ backgroundColor: backgroundColors[candlesLit] }}>
+      <BodyDiv>
         <Menorah candlesLit={candlesLit} setCandlesLit={setCandlesLit} />
         <div className="buttons">
-          <button
-            onClick={() => {
-              setCandlesLit(8);
-            }}
-          >
-            On
-          </button>
           <button
             onClick={() => {
               setCandlesLit(0);
             }}
           >
-            Off
+            0
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(1);
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(2);
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(3);
+            }}
+          >
+            3
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(4);
+            }}
+          >
+            4
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(5);
+            }}
+          >
+            5
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(6);
+            }}
+          >
+            6
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(7);
+            }}
+          >
+            7
+          </button>
+          <button
+            onClick={() => {
+              setCandlesLit(8);
+            }}
+          >
+            8
           </button>
         </div>
-      </MenorahDiv>
+      </BodyDiv>
     </AppDiv>
   );
 }
@@ -38,11 +96,12 @@ function App() {
 const AppDiv = styled.div`
   /* border: 3px dashed orange; */
   height: 100vh;
+  /* background-color: #020305; */
 
   /* background-image: linear-gradient(180deg, #252a26, #1c211d); */
 `;
 
-const MenorahDiv = styled.div`
+const BodyDiv = styled.div`
   /* height: 80%; */
   /* max-width: 100vw; */
   display: flex;
--- src/components/Menorah.jsx
+++ src/components/Menorah.jsx
@@ -3,24 +3,78 @@ import React from "react";
 import styled from "styled-components";
 
 //images
-import MenorahEmpty from "../images/Menorah-empty.svg";
-import MenorahFull from "../images/Menorah-full.svg";
+import Menorah0 from "../images/Menorah-0.svg";
+import Menorah1 from "../images/Menorah-1.svg";
+import Menorah2 from "../images/Menorah-2.svg";
+import Menorah3 from "../images/Menorah-3.svg";
+import Menorah4 from "../images/Menorah-4.svg";
+import Menorah5 from "../images/Menorah-5.svg";
+import Menorah6 from "../images/Menorah-6.svg";
+import Menorah7 from "../images/Menorah-7.svg";
+import Menorah8 from "../images/Menorah-8.svg";
 
-function Menorah({ candlesLit, setCandlesLit }) {
+function Menorah({ candlesLit }) {
   switch (candlesLit) {
     case 0:
       return (
         <>
-          {/* <img src={MenorahFull} alt="all candles lit"></img> */}
-          <img src={MenorahEmpty} alt="no candles lit"></img>
+          <img src={Menorah0} alt="no candles lit"></img>
+        </>
+      );
+
+    case 1:
+      return (
+        <>
+          <img src={Menorah1} alt="one candle lit"></img>
+        </>
+      );
+
+    case 2:
+      return (
+        <>
+          <img src={Menorah2} alt="two candles lit"></img>
+        </>
+      );
+
+    case 3:
+      return (
+        <>
+          <img src={Menorah3} alt="three candles lit"></img>
+        </>
+      );
+
+    case 4:
+      return (
+        <>
+          <img src={Menorah4} alt="four candles lit"></img>
+        </>
+      );
+
+    case 5:
+      return (
+        <>
+          <img src={Menorah5} alt="five candles lit"></img>
+        </>
+      );
+
+    case 6:
+      return (
+        <>
+          <img src={Menorah6} alt="six candles lit"></img>
+        </>
+      );
+
+    case 7:
+      return (
+        <>
+          <img src={Menorah7} alt="seven candles lit"></img>
         </>
       );
 
     case 8:
       return (
         <>
-          <img src={MenorahFull} alt="all candles lit"></img>
-          {/* <img src={MenorahEmpty} alt="no candles lit"></img> */}
+          <img src={Menorah8} alt="all eight candles lit"></img>
         </>
       );
   }
