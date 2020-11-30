4389902be84bbd0121989e7fce71a4c304ef08b1
Jude Clark <judeclark2019@gmail.com>
2020-11-30 18:27:06 -0500

    styled buttons and FLIPPED the menorah the right way round omg

--- src/App.js
+++ src/App.js
@@ -12,17 +12,35 @@ function App() {
   const [shamashLit, setShamashLit] = useState(false);
   const [shamashActive, setShamashActive] = useState(false);
 
+  // const backgroundColors = [
+  //   "#020305",
+  //   "#040609",
+  //   "#080C12",
+  //   "#101823",
+  //   "#182434",
+  //   "#1F3045",
+  //   "#26374B",
+  //   "#2D3D51",
+  //   "#374659",
+  // ];
+
   const backgroundColors = [
-    "#020305",
+    "#010102",
     "#040609",
-    "#080C12",
-    "#101823",
+    "#070a0f",
+    "#0A0F15",
+    "#0D131B",
+    "#131C28",
+    "#16202E",
     "#182434",
-    "#1F3045",
-    "#26374B",
-    "#2D3D51",
-    "#374659",
   ];
+
+  const buttonVariants = {
+    bobbing: {
+      scale: [1.2, 1],
+      transition: { yoyo: Infinity, duration: 0.2 },
+    },
+  };
 
   return (
     <AppDiv
@@ -41,99 +59,111 @@ function App() {
           shamashActive={shamashActive}
           setShamashActive={setShamashActive}
         />
-        <motion.div
+        <ButtonDiv
           className="buttons"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { duration: 1 } }}
         >
-          <button
+          <StyledButton
+            className={
+              candlesLit === 0 && shamashLit === false ? "active-btn" : ""
+            }
+            onClick={() => {
+              setCandlesLit(0);
+              setShamashLit(false);
+            }}
+          >
+            לכבות
+            <br />
+            <span>Extinguish</span>
+          </StyledButton>
+          <StyledButton
+            // variants={buttonVariants}
+            // animate="bobbing"
+            className={shamashLit ? "active-btn" : ""}
             onClick={() => {
               setShamashLit(!shamashLit);
             }}
           >
-            Shamash
-          </button>
+            שמש <br />
+            <span>Shamash</span>
+          </StyledButton>
           <br />
-          <button
-            onClick={() => {
-              setCandlesLit(0);
-            }}
-          >
-            0
-          </button>
-          <button
-            onClick={() => {
-              setCandlesLit(1);
-            }}
-          >
-            1
-          </button>
-          <button
-            onClick={() => {
-              setCandlesLit(2);
-            }}
-          >
-            2
-          </button>
-          <button
-            onClick={() => {
-              setCandlesLit(3);
-            }}
-          >
-            3
-          </button>
-          <button
-            onClick={() => {
-              setCandlesLit(4);
-            }}
-          >
-            4
-          </button>
-          <button
-            onClick={() => {
-              setCandlesLit(5);
-            }}
-          >
-            5
-          </button>
-          <button
-            onClick={() => {
-              setCandlesLit(6);
-            }}
-          >
-            6
-          </button>
-          <button
-            onClick={() => {
-              setCandlesLit(7);
-            }}
-          >
-            7
-          </button>
-          <button
+          <br />
+          <StyledButton
+            className={candlesLit >= 8 ? "active-btn" : ""}
             onClick={() => {
               setCandlesLit(8);
             }}
           >
-            8
-          </button>
-
-          {/* <button
+            ח<br />
+            <span>8</span>
+          </StyledButton>
+          <StyledButton
+            className={candlesLit >= 7 ? "active-btn" : ""}
             onClick={() => {
-              setCandlesLit(false);
+              setCandlesLit(7);
             }}
           >
-            off
-          </button>
-
-          <button
+            ז<br />
+            <span>7</span>
+          </StyledButton>
+          <StyledButton
+            className={candlesLit >= 6 ? "active-btn" : ""}
             onClick={() => {
-              setCandlesLit(true); //
+              setCandlesLit(6);
             }}
           >
-            on
-          </button> */}
-        </motion.div>
+            ו<br />
+            <span>6</span>
+          </StyledButton>
+          <StyledButton
+            className={candlesLit >= 5 ? "active-btn" : ""}
+            onClick={() => {
+              setCandlesLit(5);
+            }}
+          >
+            ה<br />
+            <span>5</span>
+          </StyledButton>
+          <StyledButton
+            className={candlesLit >= 4 ? "active-btn" : ""}
+            onClick={() => {
+              setCandlesLit(4);
+            }}
+          >
+            ד<br />
+            <span>4</span>
+          </StyledButton>
+
+          <StyledButton
+            className={candlesLit >= 3 ? "active-btn" : ""}
+            onClick={() => {
+              setCandlesLit(3);
+            }}
+          >
+            ג<br />
+            <span>3</span>
+          </StyledButton>
+          <StyledButton
+            className={candlesLit >= 2 ? "active-btn" : ""}
+            onClick={() => {
+              setCandlesLit(2);
+            }}
+          >
+            ב<br />
+            <span>2</span>
+          </StyledButton>
+          <StyledButton
+            className={candlesLit >= 1 ? "active-btn" : ""}
+            onClick={() => {
+              setCandlesLit(1);
+            }}
+          >
+            א<br />
+            <span>1</span>
+          </StyledButton>
+        </ButtonDiv>
       </BodyDiv>
     </AppDiv>
   );
@@ -144,8 +174,6 @@ const AppDiv = styled.div`
 `;
 
 const BodyDiv = styled(motion.div)`
-  /* height: 80%; */
-  /* max-width: 100vw; */
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -153,7 +181,6 @@ const BodyDiv = styled(motion.div)`
   padding: 3rem;
   height: 100%;
   width: 100%;
-  /* border: 2px solid red; */
 
   @media (max-width: 600px) {
     padding: 5rem 0rem;
@@ -163,8 +190,45 @@ const BodyDiv = styled(motion.div)`
     height: 100%;
     object-fit: cover;
   }
-  button {
-    padding: 1rem;
+`;
+
+const ButtonDiv = styled(motion.div)`
+  text-align: center;
+`;
+
+const StyledButton = styled(motion.button)`
+  font-size: 2.5rem;
+  padding: 0.4rem 0.7rem;
+  background-color: transparent;
+  color: #bc9357;
+  margin: 5px;
+  border: 2px outset #bc9357;
+  border-radius: 0.5rem;
+
+  span {
+    font-size: 1.8rem;
+  }
+
+  &.active-btn {
+    border: 2px inset #bc9357;
+    background-color: rgba(150, 116, 68, 0.1);
+    text-shadow: 0px 0px 5px rgba(254, 254, 222, 0.1),
+      0px 0px 10px rgba(254, 254, 222, 0.1),
+      0px 0px 15px rgba(254, 254, 222, 0.1);
+    box-shadow: 0px 0px 5px rgba(254, 254, 222, 0.1),
+      0px 0px 10px rgba(254, 254, 222, 0.1),
+      0px 0px 15px rgba(254, 254, 222, 0.1);
+  }
+
+  &:hover {
+    color: #ddab6f;
+    cursor: pointer;
+    text-shadow: 0px 0px 5px rgba(254, 254, 222, 0.1),
+      0px 0px 10px rgba(254, 254, 222, 0.1),
+      0px 0px 15px rgba(254, 254, 222, 0.1);
+    box-shadow: 0px 0px 5px rgba(254, 254, 222, 0.1),
+      0px 0px 10px rgba(254, 254, 222, 0.1),
+      0px 0px 15px rgba(254, 254, 222, 0.1);
   }
 `;
 
